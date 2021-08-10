7accc7ff9c0773fd7709ca1b7a5c21cc291ffef9
Norizon <69400842+norinorin@users.noreply.github.com>
2021-08-10 08:14:43 +0700

    Adjust gaps

--- config.h
+++ config.h
@@ -3,10 +3,10 @@
 /* appearance */
 static const unsigned int borderpx = 3;		  /* border pixel of windows */
 static const unsigned int snap = 32;		  /* snap pixel */
-static const unsigned int gappih = 10;		  /* horiz inner gap between windows */
-static const unsigned int gappiv = 10;		  /* vert inner gap between windows */
-static const unsigned int gappoh = 10;		  /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov = 10;		  /* vert outer gap between windows and screen edge */
+static const unsigned int gappih = 2;		  /* horiz inner gap between windows */
+static const unsigned int gappiv = 2;		  /* vert inner gap between windows */
+static const unsigned int gappoh = 14;		  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov = 14;		  /* vert outer gap between windows and screen edge */
 static const int smartgaps = 0;				  /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;  /* 0: systray in the right corner, >0: systray on left of status text */
@@ -90,8 +90,8 @@ static Key keys[] = {
 	{MODKEY, XK_d, incnmaster, {.i = -1}},
 	{MODKEY, XK_h, setmfact, {.f = -0.05}},
 	{MODKEY, XK_l, setmfact, {.f = +0.05}},
-	{MODKEY, XK_z, incrgaps, {.i = +1}},
-	{MODKEY, XK_x, incrgaps, {.i = -1}},
+	{MODKEY, XK_z, incrgaps, {.i = +5}},
+	{MODKEY, XK_x, incrgaps, {.i = -5}},
 	// {MODKEY | ShiftMask, XK_h, incrogaps, {.i = +1}},
 	// {MODKEY | ShiftMask, XK_l, incrogaps, {.i = -1}},
 	// {MODKEY | ControlMask, XK_h, incrigaps, {.i = +1}},
