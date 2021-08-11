802d50ce21fd3a87697c3456c0052ff5065ed0b2
Norizon <69400842+norinorin@users.noreply.github.com>
2021-08-11 22:49:20 +0700

    Add gaps to the monocle layout

--- dwm.c
+++ dwm.c
@@ -1465,7 +1465,7 @@ void monocle(Monitor *m)
 	if (n > 0) /* override layout symbol */
 		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
 	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
-		resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+		resize(c, m->wx + m->gappoh, m->wy + m->gappov, m->ww - 2 * c->bw - 2 * m->gappoh, m->wh - 2 * c->bw - 2 * m->gappov, 0);
 }
 
 void motionnotify(XEvent *e)
