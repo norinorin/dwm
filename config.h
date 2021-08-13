aedcc020418f3cf3520a3ef3a6c2f73bbdd26748
Norizon <69400842+norinorin@users.noreply.github.com>
2021-08-13 10:41:56 +0700

    Adjust bar padding

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const int systraypinningfailfirst = 1; /* 1: if pinning fails, display sy
 static const int showsystray = 1;			  /* 0 means no systray */
 static const int showbar = 1;				  /* 0 means no bar */
 static const int topbar = 1;				  /* 0 means bottom bar */
-static const int barhgap = 8;
+static const int barhgap = 10;
 static const char *fonts[] = {"SauceCodePro Nerd Font Mono:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"};
 static const char dmenufont[] = "monospace:size=10";
 static const char normbgcolor[] = "#2E3440";
